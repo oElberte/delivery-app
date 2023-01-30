6dab7c1c4cffbd326cd9f1069bbea646fa6ca0b9
Elberte Plinio <elbertedoolan@gmail.com>
2023-01-30 18:23:44 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/elberte/dartweek9/dw9_delivery_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.elberte.dartweek9.dw9_delivery_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+import 'app/dw9_delivery_app.dart';
+
+void main() {
+  runApp(const Dw9DeliveryApp());
+}
