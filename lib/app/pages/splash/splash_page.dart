e17bf4a76a00964d8752ebef9e20a277f4ee0811
Elberte Plinio <elbertedoolan@gmail.com>
2023-01-31 11:53:41 -0300

    added environment variables

--- lib/app/pages/splash/splash_page.dart
+++ lib/app/pages/splash/splash_page.dart
@@ -1,3 +1,4 @@
+import 'package:dw9_delivery_app/app/core/config/env/env.dart';
 import 'package:dw9_delivery_app/app/core/ui/widgets/delivery_button.dart';
 import 'package:flutter/material.dart';
 
@@ -15,7 +16,7 @@ class SplashPage extends StatelessWidget {
           Container(),
           DeliveryButton(
             onPressed: () {},
-            label: 'Teste Label',
+            label: Env.i['backend_base_url'] ?? '',
           ),
           TextFormField(
             decoration: InputDecoration(labelText: 'text'),
