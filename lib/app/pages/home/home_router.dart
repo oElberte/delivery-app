4e15eddaa5dbc148a0946fa06963a04d4741e1ad
Elberte Plinio <elbertedoolan@gmail.com>
2023-01-31 15:16:40 -0300

    added base state

--- lib/app/pages/home/home_router.dart
+++ lib/app/pages/home/home_router.dart
@@ -1,9 +1,10 @@
-import 'package:dw9_delivery_app/app/pages/home/home_controller.dart';
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../../repositories/products/products_repository.dart';
 import '../../repositories/products/products_repository_impl.dart';
+import 'home_controller.dart';
 import 'home_page.dart';
 
 class HomeRouter {
