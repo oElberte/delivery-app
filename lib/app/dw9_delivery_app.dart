10da14b4819b688477018072eb9d2b07d1c10775
Elberte Plinio <elbertedoolan@gmail.com>
2023-01-31 12:51:09 -0300

    added custom dio provider

--- lib/app/dw9_delivery_app.dart
+++ lib/app/dw9_delivery_app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'core/provider/application_binding.dart';
 import 'core/ui/theme/theme_config.dart';
 import 'pages/splash/splash_page.dart';
 
@@ -8,12 +9,14 @@ class Dw9DeliveryApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Delivery App',
-      theme: ThemeConfig.theme,
-      routes: {
-        '/': (context) => const SplashPage(),
-      },
+    return ApplicationBinding(
+      child: MaterialApp(
+        title: 'Delivery App',
+        theme: ThemeConfig.theme,
+        routes: {
+          '/': (context) => const SplashPage(),
+        },
+      ),
     );
   }
 }
