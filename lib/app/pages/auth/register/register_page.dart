e7e20f904639cbba9a142449e0e3d798868a62c1
Elberte Plinio <elbertedoolan@gmail.com>
2023-02-06 12:26:31 -0300

    register page finished

--- lib/app/pages/auth/register/register_page.dart
+++ lib/app/pages/auth/register/register_page.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:validatorless/validatorless.dart';
 
+import '../../../core/ui/base_state/base_state.dart';
 import '../../../core/ui/styles/text_styles.dart';
 import '../../../core/ui/widgets/delivery_app_bar.dart';
 import '../../../core/ui/widgets/delivery_button.dart';
+import 'register_controller.dart';
+import 'register_state.dart';
 
 class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
@@ -12,7 +16,7 @@ class RegisterPage extends StatefulWidget {
   State<RegisterPage> createState() => _RegisterPageState();
 }
 
-class _RegisterPageState extends State<RegisterPage> {
+class _RegisterPageState extends BaseState<RegisterPage, RegisterController> {
   final _formKey = GlobalKey<FormState>();
 
   final _nameEC = TextEditingController();
@@ -29,73 +33,96 @@ class _RegisterPageState extends State<RegisterPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: DeliveryAppBar(),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Cadastro',
-                  style: context.textStyles.textTitle,
-                ),
-                Text(
-                  'Preencha os campos abaixo para criar o seu cadastro',
-                  style: context.textStyles.textMedium.copyWith(fontSize: 18),
-                ),
-                const SizedBox(height: 30),
-                TextFormField(
-                  decoration: const InputDecoration(labelText: 'Nome'),
-                  controller: _nameEC,
-                  validator: Validatorless.required('Nome obrigatório'),
-                ),
-                const SizedBox(height: 30),
-                TextFormField(
-                  decoration: const InputDecoration(labelText: 'E-mail'),
-                  controller: _emailEC,
-                  validator: Validatorless.multiple([
-                    Validatorless.required('E-mail obrigatório'),
-                    Validatorless.email('E-mail inválido'),
-                  ]),
-                ),
-                const SizedBox(height: 30),
-                TextFormField(
-                  decoration: const InputDecoration(labelText: 'Senha'),
-                  controller: _passwordEC,
-                  validator: Validatorless.multiple([
-                    Validatorless.required('Senha obrigatória'),
-                    Validatorless.min(6, 'Mínimo de 6 caracteres'),
-                  ]),
-                  obscureText: true,
-                ),
-                const SizedBox(height: 30),
-                TextFormField(
-                  decoration:
-                      const InputDecoration(labelText: 'Confirmar senha'),
-                  validator: Validatorless.multiple([
-                    Validatorless.required('Confirmação de senha obrigatória'),
-                    Validatorless.compare(_passwordEC, 'Senhas não conferem'),
-                  ]),
-                  obscureText: true,
-                ),
-                const SizedBox(height: 30),
-                Center(
-                  child: DeliveryButton(
-                    label: 'CADASTRAR',
-                    onPressed: () {
-                      final valid = _formKey.currentState?.validate() ?? false;
-                      if (valid) {
-                        
-                      }
-                    },
-                    width: double.infinity,
+    return BlocListener<RegisterController, RegisterState>(
+      listener: (context, state) {
+        state.status.matchAny(
+          any: () => hideLoader(),
+          register: () => showLoader(),
+          error: () {
+            hideLoader();
+            showError('Erro ao registrar o usuário');
+          },
+          success: () {
+            hideLoader();
+            showSuccess('Cadastro realizado com sucesso');
+            Navigator.pop(context);
+          },
+        );
+      },
+      child: Scaffold(
+        appBar: DeliveryAppBar(),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Cadastro',
+                    style: context.textStyles.textTitle,
                   ),
-                ),
-              ],
+                  Text(
+                    'Preencha os campos abaixo para criar o seu cadastro',
+                    style: context.textStyles.textMedium.copyWith(fontSize: 18),
+                  ),
+                  const SizedBox(height: 30),
+                  TextFormField(
+                    decoration: const InputDecoration(labelText: 'Nome'),
+                    controller: _nameEC,
+                    validator: Validatorless.required('Nome obrigatório'),
+                  ),
+                  const SizedBox(height: 30),
+                  TextFormField(
+                    decoration: const InputDecoration(labelText: 'E-mail'),
+                    controller: _emailEC,
+                    validator: Validatorless.multiple([
+                      Validatorless.required('E-mail obrigatório'),
+                      Validatorless.email('E-mail inválido'),
+                    ]),
+                  ),
+                  const SizedBox(height: 30),
+                  TextFormField(
+                    decoration: const InputDecoration(labelText: 'Senha'),
+                    controller: _passwordEC,
+                    validator: Validatorless.multiple([
+                      Validatorless.required('Senha obrigatória'),
+                      Validatorless.min(6, 'Mínimo de 6 caracteres'),
+                    ]),
+                    obscureText: true,
+                  ),
+                  const SizedBox(height: 30),
+                  TextFormField(
+                    decoration:
+                        const InputDecoration(labelText: 'Confirmar senha'),
+                    validator: Validatorless.multiple([
+                      Validatorless.required(
+                          'Confirmação de senha obrigatória'),
+                      Validatorless.compare(_passwordEC, 'Senhas não conferem'),
+                    ]),
+                    obscureText: true,
+                  ),
+                  const SizedBox(height: 30),
+                  Center(
+                    child: DeliveryButton(
+                      label: 'CADASTRAR',
+                      onPressed: () {
+                        final valid =
+                            _formKey.currentState?.validate() ?? false;
+                        if (valid) {
+                          controller.register(
+                            _nameEC.text,
+                            _emailEC.text,
+                            _passwordEC.text,
+                          );
+                        }
+                      },
+                      width: double.infinity,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
