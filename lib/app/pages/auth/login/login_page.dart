cf0f6dbea7ee16e8f90c017fd527596f063649a2
Elberte Plinio <elbertedoolan@gmail.com>
2023-02-10 20:25:02 -0300

    login page finished

--- lib/app/pages/auth/login/login_page.dart
+++ lib/app/pages/auth/login/login_page.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:validatorless/validatorless.dart';
 
+import '../../../core/ui/base_state/base_state.dart';
 import '../../../core/ui/styles/text_styles.dart';
 import '../../../core/ui/widgets/delivery_app_bar.dart';
 import '../../../core/ui/widgets/delivery_button.dart';
+import 'login_controller.dart';
+import 'login_state.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -12,7 +16,7 @@ class LoginPage extends StatefulWidget {
   State<LoginPage> createState() => _LoginPageState();
 }
 
-class _LoginPageState extends State<LoginPage> {
+class _LoginPageState extends BaseState<LoginPage, LoginController> {
   final _formKey = GlobalKey<FormState>();
   final _emailEC = TextEditingController();
   final _passwordEC = TextEditingController();
@@ -26,85 +30,107 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: DeliveryAppBar(),
-      body: CustomScrollView(
-        slivers: [
-          SliverToBoxAdapter(
-            child: Padding(
-              padding: const EdgeInsets.all(20),
-              child: Form(
-                key: _formKey,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('Login', style: context.textStyles.textTitle),
-                    const SizedBox(height: 30),
-                    TextFormField(
-                      decoration: const InputDecoration(
-                        label: Text('E-mail'),
+    return BlocListener<LoginController, LoginState>(
+      listener: (context, state) {
+        state.status.matchAny(
+          any: () => hideLoader(),
+          login: () => showLoader(),
+          loginError: () {
+            hideLoader();
+            showError('E-mail ou senha inválidos');
+          },
+          error: () {
+            hideLoader();
+            showError('Erro ao realizar login');
+          },
+          success: () {
+            hideLoader();
+            Navigator.pop(context, true);
+          },
+        );
+      },
+      child: Scaffold(
+        appBar: DeliveryAppBar(),
+        body: CustomScrollView(
+          slivers: [
+            SliverToBoxAdapter(
+              child: Padding(
+                padding: const EdgeInsets.all(20),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Login', style: context.textStyles.textTitle),
+                      const SizedBox(height: 30),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          label: Text('E-mail'),
+                        ),
+                        validator: Validatorless.multiple([
+                          Validatorless.required('E-mail obrigatório'),
+                          Validatorless.email('E-mail inválido'),
+                        ]),
+                        controller: _emailEC,
                       ),
-                      validator: Validatorless.multiple([
-                        Validatorless.required('E-mail obrigatório'),
-                        Validatorless.email('E-mail inválido'),
-                      ]),
-                      controller: _emailEC,
-                    ),
-                    const SizedBox(height: 30),
-                    TextFormField(
-                      decoration: const InputDecoration(
-                        label: Text('Senha'),
+                      const SizedBox(height: 30),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          label: Text('Senha'),
+                        ),
+                        validator: Validatorless.required('Senha obrigatória'),
+                        controller: _passwordEC,
+                        obscureText: true,
                       ),
-                      validator: Validatorless.required('Senha obrigatória'),
-                      controller: _passwordEC,
-                    ),
-                    const SizedBox(height: 50),
-                    Center(
-                      child: DeliveryButton(
-                        width: double.infinity,
-                        label: 'ENTRAR',
-                        onPressed: () {
-                          final valid =
-                              _formKey.currentState?.validate() ?? false;
-                          if (valid) {}
-                        },
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-          SliverFillRemaining(
-            hasScrollBody: false,
-            child: Padding(
-              padding: const EdgeInsets.all(8),
-              child: Align(
-                alignment: Alignment.bottomCenter,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Não possui uma conta? ',
-                      style: context.textStyles.textBold,
-                    ),
-                    TextButton(
-                      onPressed: () {
-                        Navigator.of(context).pushNamed('/auth/register');
-                      },
-                      child: Text(
-                        'Cadastre-se!',
-                        style: context.textStyles.textBold.copyWith(
-                          color: Colors.blue,
+                      const SizedBox(height: 50),
+                      Center(
+                        child: DeliveryButton(
+                          width: double.infinity,
+                          label: 'ENTRAR',
+                          onPressed: () {
+                            final valid = _formKey.currentState?.validate() ?? false;
+                            if (valid) {
+                              controller.login(_emailEC.text, _passwordEC.text);
+                            }
+                          },
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
+            SliverFillRemaining(
+              hasScrollBody: false,
+              child: Padding(
+                padding: const EdgeInsets.all(8),
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Não possui uma conta? ',
+                        style: context.textStyles.textBold,
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.of(context).pushNamed('/auth/register');
+                        },
+                        child: Text(
+                          'Cadastre-se!',
+                          style: context.textStyles.textBold.copyWith(
+                            color: Colors.blue,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
