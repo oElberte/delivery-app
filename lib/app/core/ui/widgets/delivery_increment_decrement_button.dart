b058295fbd22ff02f5f333ae091690d45b95a030
Elberte Plinio <elbertedoolan@gmail.com>
2023-02-13 11:10:50 -0300

    added increment decrement button compact

--- lib/app/core/ui/widgets/delivery_increment_decrement_button.dart
+++ lib/app/core/ui/widgets/delivery_increment_decrement_button.dart
@@ -5,6 +5,7 @@ import '../../../core/ui/styles/colors_app.dart';
 import '../../../core/ui/styles/text_styles.dart';
 
 class DeliveryIncrementDecrementButton extends StatelessWidget {
+  final bool _compact;
   final int amount;
   final VoidCallback incrementTap;
   final VoidCallback decrementTap;
@@ -14,11 +15,19 @@ class DeliveryIncrementDecrementButton extends StatelessWidget {
     required this.amount,
     required this.incrementTap,
     required this.decrementTap,
-  });
+  }) : _compact = false;
+
+  const DeliveryIncrementDecrementButton.compact({
+    super.key,
+    required this.amount,
+    required this.incrementTap,
+    required this.decrementTap,
+  }) : _compact = true;
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: _compact ? const EdgeInsets.all(5) : null,
       decoration: BoxDecoration(
         border: Border.all(color: Colors.grey),
         borderRadius: BorderRadius.circular(7),
@@ -33,7 +42,7 @@ class DeliveryIncrementDecrementButton extends StatelessWidget {
               child: Text(
                 '-',
                 style: context.textStyles.textMedium.copyWith(
-                  fontSize: 22,
+                  fontSize: _compact ? 10 : 22,
                   color: Colors.grey,
                 ),
               ),
@@ -42,7 +51,7 @@ class DeliveryIncrementDecrementButton extends StatelessWidget {
           Text(
             amount.toString(),
             style: context.textStyles.textRegular.copyWith(
-              fontSize: 17,
+              fontSize: _compact ? 13 : 17,
               color: context.colors.secondary,
             ),
           ),
@@ -53,7 +62,7 @@ class DeliveryIncrementDecrementButton extends StatelessWidget {
               child: Text(
                 '+',
                 style: context.textStyles.textMedium.copyWith(
-                  fontSize: 22,
+                  fontSize: _compact ? 10 : 22,
                   color: context.colors.secondary,
                 ),
               ),
