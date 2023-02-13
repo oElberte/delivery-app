72bcecc5db7d6faa015f3246d2f52fd1b3a6bff3
Elberte Plinio <elbertedoolan@gmail.com>
2023-02-13 16:23:48 -0300

    added order completed screen

--- lib/app/pages/order/order_completed_page.dart
+++ lib/app/pages/order/order_completed_page.dart
@@ -1,15 +1,38 @@
 import 'package:flutter/material.dart';
 
+import '../../core/ui/helpers/size_extensions.dart';
+import '../../core/ui/styles/text_styles.dart';
+import '../../core/ui/widgets/delivery_button.dart';
+
 class OrderCompletedPage extends StatelessWidget {
   const OrderCompletedPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(''),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(height: context.percentHeight(.2)),
+              Image.asset('assets/images/logo_rounded.png'),
+              const SizedBox(height: 20),
+              Text(
+                'Pedido realizado com sucesso, em breve você receberá a confirmacão do seu pedido',
+                textAlign: TextAlign.center,
+                style: context.textStyles.textExtraBold.copyWith(fontSize: 18),
+              ),
+              const SizedBox(height: 20),
+              DeliveryButton(
+                width: context.percentWidth(.8),
+                label: 'FECHAR',
+                onPressed: () => Navigator.pop(context),
+              ),
+            ],
+          ),
+        ),
       ),
-      body: Text('Sucesso!'),
     );
   }
 }
